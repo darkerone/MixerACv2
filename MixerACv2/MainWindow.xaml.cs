526967b1852c16071ea7f4704f46785c8f7005bc
chaupinan <antoine.chaupin-ext@vinci-autoroutes.com>
2018-06-19 14:56:52 +0200

    Mise en place de la base du projet
    BaseViewModel
    DelegateCommand

--- MixerACv2/MainWindow.xaml.cs
+++ MixerACv2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MixerACv2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace MixerACv2
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
